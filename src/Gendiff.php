3124ba582a143509869de63b296a4d692c9a1c5c
Alexandr Petrov <yarncat@yandex.ru>
2020-09-26 23:15:15 +0400

    fixed linter errors

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -5,26 +5,26 @@ namespace DifferencesGenerator\Gendiff;
 function boolToString($value)
 {
     if (is_bool($value)) {
-		return $value ? 'true' : 'false';
-	}
-	return $value;
+        return $value ? 'true' : 'false';
+    }
+    return $value;
 }
 
 function jsonDecode($file)
 {
-	$fileToArray = json_decode(file_get_contents($file), true);
-	return array_map(function($element) {
-		return boolToString($element);
-	}, $fileToArray);
+    $fileToArray = json_decode(file_get_contents($file), true);
+    return array_map(function ($element) {
+        return boolToString($element);
+    }, $fileToArray);
 }
 
 function genDiff($firstFile, $secondFile)
 {
-	$file1 = jsonDecode($firstFile);
-	$file2 = jsonDecode($secondFile);
+    $file1 = jsonDecode($firstFile);
+    $file2 = jsonDecode($secondFile);
     $keys = array_keys(array_merge($file1, $file2));
     sort($keys);
-	
+
     $result = array_reduce($keys, function ($acc, $key) use ($file1, $file2) {
         if (array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
             if ($file1[$key] === $file2[$key]) {
