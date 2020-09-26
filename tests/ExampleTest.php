c21c90bb0be76da77fb03f4963b844881a2ed7e3
Alexandr Petrov <yarncat@yandex.ru>
2020-09-26 23:07:21 +0400

    add fixtures and example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DifferencesGenerator\tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
