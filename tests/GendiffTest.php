8fa1ecd1818e61613d3a6cca202febfcbc5090d1
Alexandr Petrov <yarncat@yandex.ru>
2020-10-01 23:42:10 +0400

    add tests for gendiff: flat (.JSON and .yaml) files comparison

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -8,10 +8,17 @@ use function DifferencesGenerator\Gendiff\genDiff;
 
 class GendiffTest extends TestCase
 {
-    public function testGendiff()
+    public function testGendiffJson()
     {
 		$expected = file_get_contents('tests/fixtures/file.txt');
-		$actual = genDiff("tests/fixtures/file1.json", "tests/fixtures/file2.json");
-        $this->expectOutputString($expected, $actual);
+		$actualJson = genDiff("tests/fixtures/file1.json", "tests/fixtures/file2.json");
+        $this->expectOutputString($expected, $actualJson);
+    }
+	
+	public function testGendiffYaml()
+    {
+		$expected = file_get_contents('tests/fixtures/file.txt');
+		$actualYaml = genDiff("tests/fixtures/file1.yml", "tests/fixtures/file2.yml");
+		$this->expectOutputString($expected, $actualYaml);
     }
 }
