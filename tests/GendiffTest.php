5143f9cb814a9329c17d926e56bcb9830eaa6c17
Alexandr Petrov <yarncat@yandex.ru>
2020-10-08 08:18:08 +0400

    add fixtures

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -8,17 +8,31 @@ use function DifferencesGenerator\Gendiff\genDiff;
 
 class GendiffTest extends TestCase
 {
-    public function testGendiffJson()
+    public function testGendiffForFlatJson()
     {
-        $expected = file_get_contents('tests/fixtures/file.txt');
-        $actualJson = genDiff('tests/fixtures/file1.json', 'tests/fixtures/file2.json');
+        $expected = file_get_contents('tests/fixtures/flatFilesDiff.txt');
+        $actualJson = genDiff('tests/fixtures/fileFlat1.json', 'tests/fixtures/fileFlat2.json');
         $this->expectOutputString($expected, $actualJson);
     }
 
-    public function testGendiffYaml()
+    public function testGendiffForFlatYaml()
     {
-        $expected = file_get_contents('tests/fixtures/file.txt');
-        $actualYaml = genDiff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml');
+        $expected = file_get_contents('tests/fixtures/flatFilesDiff.txt');
+        $actualYaml = genDiff('tests/fixtures/fileFlat1.yml', 'tests/fixtures/fileFlat2.yml');
+        $this->expectOutputString($expected, $actualYaml);
+    }
+
+    public function testGendiffForNestedJson()
+    {
+        $expected = file_get_contents('tests/fixtures/nestedFilesDiff.txt');
+        $actualJson = genDiff('tests/fixtures/fileNested1.json', 'tests/fixtures/fileNested2.json');
+        $this->expectOutputString($expected, $actualJson);
+    }
+
+    public function testGendiffForNestedYaml()
+    {
+        $expected = file_get_contents('tests/fixtures/nestedFilesDiff.txt');
+        $actualYaml = genDiff('tests/fixtures/fileNested1.yml', 'tests/fixtures/fileNested2.yml');
         $this->expectOutputString($expected, $actualYaml);
     }
 }
