1004b6e3cf04a6a5aa60bb60c9c84fc4340cb9ee
Alexandr Petrov <yarncat@yandex.ru>
2020-10-14 15:17:03 +0400

    update fixtures and tests

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -5,6 +5,8 @@ namespace Differ\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Gendiff\genDiff;
+use function Differ\Formatters\Stylish\renderStylish;
+use function Differ\Formatters\Plain\renderPlain;
 
 class GendiffTest extends TestCase
 {
@@ -24,13 +26,12 @@ class GendiffTest extends TestCase
     public function fileProvider()
     {
         return [
-            ['flatFile1.json', 'flatFile2.json', 'stylish', 'flatFilesDiff.txt'],
-            ['nestedStructure1.json', 'nestedStructure2.json', 'stylish', 'nestedStructuresDiff.txt'],
+            ['nestedStructure1.json', 'nestedStructure2.json', 'stylish', 'stylishDiff.txt'],
             ['nestedStructure1.json', 'nestedStructure2.json', 'plain', 'plainDiff.txt'],
             ['nestedStructure1.json', 'nestedStructure2.json', 'json', 'jsonDiff.txt'],
             ['composer1.json', 'composer2.json', 'stylish', 'composerDiff.txt'],
-            ['flatFile1.yaml', 'flatFile2.yaml', 'stylish', 'flatFilesDiff.txt'],
-            ['nestedStructure1.yml', 'nestedStructure2.yml', 'stylish', 'nestedStructuresDiff.txt']
+            ['nestedStructure1.yml', 'nestedStructure2.yml', 'stylish', 'stylishDiff.txt'],
+            ['nestedStructure1.yaml', 'nestedStructure2.yaml', 'stylish', 'stylishDiff.txt']
         ];
     }
 
@@ -50,7 +51,23 @@ class GendiffTest extends TestCase
             ['flatFile.json', 'flatFile2.json', 'stylish',
                 "File 'tests/fixtures/flatFile.json' is not exist or the specified path is incorrect\n"],
             ['jsonDiff.txt', 'plainDiff.txt', 'plain', "Unsupported or unknown format: 'txt'\n"],
-            ['flatFile1.json', 'flatFile2.json', 'lolkek', "Unknown output format: 'lolkek'!\n"]
+            ['nestedStructure1.yaml', 'nestedStructure2.yaml', 'lolkek', "Unknown output format: 'lolkek'!\n"]
         ];
+    }
+
+    public function testInvalidTreeRenderingStylish()
+    {
+        $data = file_get_contents(self::DIR . 'invalidTree.txt');
+        $tree = json_decode($data, true);
+        $this->expectExceptionMessage("Tree rendering error: unknown node type\n");
+        renderStylish($tree);
+    }
+
+    public function testInvalidTreeRenderingPlain()
+    {
+        $data = file_get_contents(self::DIR . 'invalidTree.txt');
+        $tree = json_decode($data, true);
+        $this->expectExceptionMessage("Tree rendering error: unknown node type\n");
+        renderPlain($tree);
     }
 }
